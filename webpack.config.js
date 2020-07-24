24b6e40aa918517184655d7a67c79657cd182683
Basnor <basnor@mail.ru>
2020-07-24 23:59:18 +0700

    set common webpuck settings

---
+++ webpack.config.js
@@ -0,0 +1,78 @@
+const path = require('path');
+const HtmlWebpackPlugin = require('html-webpack-plugin');
+const MiniCssExtractPlugin = require('mini-css-extract-plugin');
+
+const isDevelopment = process.env.NODE_ENV === 'development'
+
+
+const cssLoaders = extra => {
+    const loaders = [
+        {
+            loader: MiniCssExtractPlugin.loader,
+            options: {
+                hmr: isDevelopment,
+                reloadAll: true
+            },
+        },
+        'css-loader'
+    ]
+
+    if (extra) {
+        loaders.push(extra)
+    }
+
+    return loaders
+}
+
+const plugins = () => {
+    const base = [
+        new HtmlWebpackPlugin({
+            filename: 'index.html',
+            template: 'src/index.pug',
+            inject: true
+        }),
+        new MiniCssExtractPlugin({
+            filename: '[name].css'
+        })
+    ]
+
+    return base
+}  
+
+const config = {
+    mode: 'development',
+    entry: {
+        main: './src/app.js'
+    },
+    output: {
+        path: path.resolve(__dirname, 'dist'),
+        filename: '[name].bundle.js'
+    },
+    plugins: plugins(),
+    devtool: 'eval-cheap-source-map',
+    module: {
+        rules: [
+            { 
+                test: /\.pug$/,
+                use: ["pug-loader"]
+            },
+            {
+                test: /\.css$/,
+                use: cssLoaders()
+            },
+            {
+                test: /\.s[ac]ss$/,
+                use: cssLoaders('sass-loader')
+            }
+
+
+        ]
+    },
+    devServer: {
+        contentBase: path.join(__dirname, 'dist'),
+        port: 9000,
+        hot: isDevelopment
+    }
+}
+
+module.exports = config;
